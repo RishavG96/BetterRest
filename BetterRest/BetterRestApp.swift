4d13fb73d0de94183c5bab85d6f7f136fe15969c
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-17 09:23:21 +0530

    Initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Rishav Gupta on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
